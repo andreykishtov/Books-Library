950c5030fb6788a0d894e027a40eaa10de09e20c
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-08 19:03:39 +0200

    changed server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -19,7 +19,8 @@ const BooksWrapper = styled.div`
 `;
 
 class App extends Component {
-  componentWillMount = () => this.props.dispatch(booksRequest());
+  componentWillMount = () =>
+    this.props.dispatch(booksRequest('http://ec2-18-217-102-218.us-east-2.compute.amazonaws.com:4000'));
 
   render() {
     const { books } = this.props;
